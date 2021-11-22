9a4fb277587ae187e6e4ca450273c100a41f791d
hananessam <noona.zeroo@gmail.com>
2021-11-22 22:38:31 +0200

    header

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import bootstrap from 'bootstrap';
+
+import $ from "jquery";
+window.$ = window.jQuery = $;
+
+
+import './style.scss';
+
+$(document).ready(function () {
+	
+})
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname),
+    },
+    // compress: true,
+    port: 8080,
+  },
+  watch: true,
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+  },
+
+};
