c217e53a078b094be3beff3559820ed24c81d1de
hananessam <noona.zeroo@gmail.com>
2021-11-24 21:51:20 +0200

    add testimonial

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import bootstrap from 'bootstrap';
 
 import $ from "jquery";
 window.$ = window.jQuery = $;
+import Slick from "slick-carousel";
 
 
 import './style.scss';
 
 $(document).ready(function () {
-	
+	$('.testimonial').slick({
+		rtl: true
+	});
 })
