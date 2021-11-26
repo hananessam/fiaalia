4c297d12aea7af1214e36317a74e70125fffb0e1
hananessam <noona.zeroo@gmail.com>
2021-11-26 22:09:14 +0200

    add ticket page

--- src/app.js
+++ src/app.js
@@ -11,4 +11,26 @@ $(document).ready(function () {
 	$('.testimonial').slick({
 		rtl: true
 	});
+
+
+	let count = 1;
+	let price = 300.00;
+
+	$('.counter .num').html(count);
+
+	$('.counter .inc').click(function () {
+		if (count >= 10) return;
+		count++;
+		$('.counter .num').html(count);
+		$('.item-price').html((count*price).toFixed(2))
+		$('.total').html((count*price).toFixed(2))
+	})
+
+	$('.counter .dec').click(function () {
+		if (count <= 1) return;
+		count--;
+		$('.counter .num').html(count);
+		$('.item-price').html((count*price).toFixed(2))
+		$('.total').html((count*price).toFixed(2))
+	})
 })
