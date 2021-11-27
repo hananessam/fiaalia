60aec171b7a0d218049d0d325022cff5df1c1e5a
hananessam <noona.zeroo@gmail.com>
2021-11-27 11:35:05 +0200

    add confirmation page

--- src/app.js
+++ src/app.js
@@ -33,4 +33,26 @@ $(document).ready(function () {
 		$('.item-price').html((count*price).toFixed(2))
 		$('.total').html((count*price).toFixed(2))
 	})
+
+
+
+	const goToNextInput = (e) => {
+    var key = e.which;
+    var t = $(e.target);
+    var next = t.next('input');
+    var prev = t.prev('input');
+
+    if (event.keyCode == 8 || event.keyCode == 46) {
+    	prev.select().focus();
+    }else{
+    	next.select().focus();
+    }
+  }
+  
+  const onFocus = (e) => {
+    $(e.target).select();
+  }
+
+  $('body').on('keyup', 'input', goToNextInput);
+  $('body').on('click', 'input', onFocus);
 })
